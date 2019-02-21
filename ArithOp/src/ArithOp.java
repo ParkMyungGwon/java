5a30a64abe5ae86efd087c42d3fd2fe4ca5ba915
ParkMyungGwon <rotjdgks@naver.com>
2019-02-21 21:25:43 +0900

    ArithOp 예제 연습

---
+++ ArithOp/src/ArithOp.java
@@ -0,0 +1,16 @@
+
+public class ArithOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 10;
+		int n2 = 3;
+		
+		int result = n1 + n2;
+		
+		System.out.println("나눗셈 결과" + n1 / n2);
+		
+		System.out.println("나머지 결과" + n1 % n2);
+	}
+
+}
