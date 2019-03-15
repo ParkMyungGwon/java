cdac2fde34773b6f85bba71185046e7bcda03096
ParkMyungGwon <rotjdgks@naver.com>
2019-03-16 00:31:55 +0900

    배열 실습 정리

---
+++ EnhancedFor/src/EnhancedFor.java
@@ -0,0 +1,20 @@
+
+public class EnhancedFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		int sum = 0;
+		
+		for(int e : arr){
+			sum += e;
+		}
+		
+		System.out.println(sum);
+		
+		
+	}
+
+}
