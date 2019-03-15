5b9c6b1b48017461c8f71464f5b0a740c649f48e
ParkMyungGwon <rotjdgks@naver.com>
2019-03-15 21:51:52 +0900

    배열 실습2

---
+++ InstanceArray/src/InstanceArray.java
@@ -0,0 +1,17 @@
+
+public class InstanceArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] strArr = new String[3];
+		strArr[0] = new String("java");
+		strArr[1] = new String("dlewx");
+		strArr[2] = new String("Rudy");
+		
+		for(int i=0; i <strArr.length ; i++){
+			System.out.println(strArr[i]);
+		}
+	}
+
+}
